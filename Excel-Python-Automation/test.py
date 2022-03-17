4b6882f9deba65d04c0f573d5dbb46d4c94f17a5
rk1091 <riya_kapoor@hotmail.com>
2022-03-17 23:06:18 +0530

    Final Project Files

--- Excel-Python-Automation/test.py
+++
@@ -1,9 +0,0 @@
-
-#import pandas as pd
-
-
-#rx=pd.read_excel('OUTPUT SHEET.xlsx',sheet_name='Sheet1')
-#print(rx)
-
-#ry=pd.read_excel('ANSWER_SHEET with Answers (INPUT SHEET).xlsx',sheet_name='Sheet1')
-print('ry')
--- Excel-Python-Automation/try1/tempCodeRunnerFile.py
+++
@@ -1,2 +0,0 @@
-except TypeError:
-#     print('Please make sure that the input excel files have data according to the correct format (i.e apart from name, input only numbers elsewhere).')
